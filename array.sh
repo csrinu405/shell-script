0afa0300c7b7f7c90740f9523bb788bf0af1cc51
srinivas <csrinu405@yahoo.com>
2024-08-07 09:09:28 +0530

    array1

--- array.sh
+++ array.sh
@@ -4,5 +4,5 @@ names=("seenu" "swathi" "parthiv")
 
 echo "my son name is" ${names[2]}
 echo "my wife name is" ${names[1]}
-echo "my name is" ${names[1]}
+echo "my name is" ${names[0]}
 echo "my family name is" ${names[@]}
