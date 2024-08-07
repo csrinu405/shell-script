fdbd098a1636f32d639984825170b44999fc4f27
srinivas <csrinu405@yahoo.com>
2024-08-07 20:20:18 +0530

    script

--- if.sh
+++ if.sh
@@ -2,7 +2,7 @@
 
 if [ $1 -gt 20 ]
 then
-    echo "provided number is greater than $1"
+    echo "provided number is greater than 20"
 else
-    echo "provided number is lesser than $1"
+    echo "provided number is lesser than 20"
 fi
