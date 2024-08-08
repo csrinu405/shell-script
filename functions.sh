c87914b36bf00e6ed17dc729afef799d56a1b078
Srinivas Chandupatla <srch0821@netcracker.com>
2024-08-08 17:30:17 +0530

    functions

--- functions.sh
+++ functions.sh
@@ -7,12 +7,20 @@ user=$(id -u)
 if [ $user -ne 0 ]
 then
     echo "you are a normal user"
-    exit 1
 else
     echo "you are root user"
 fi
 yum install mysql -y
 
+if [ $? -eq 0 ]
+then 
+    echo "package is installed"
+else
+    echo "package is not installed"
 yum install openssh -y
 
-echo $?
+if [ $? -eq 0]
+then
+    echo "package installed"
+else
+    echo "package not installed"
