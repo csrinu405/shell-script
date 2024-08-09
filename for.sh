c876b336937c8b3b9e4a934bf6fa48042d3f72fc
Srinivas Chandupatla <srch0821@netcracker.com>
2024-08-09 19:24:59 +0530

    functions

--- for.sh
+++ for.sh
@@ -4,6 +4,13 @@ user=$(id -u)
 time=$(date +%F-%H-%M-%S )
 logfile=$(echo $0|cut -d "." -f1)
 
+if [ $user -ne 0 ]
+then
+    echo "you are not a root user"
+else
+    echo "you are a root user and proceeding"
+fi
+
 for i in $@
 do 
   yum list installed |grep $i
